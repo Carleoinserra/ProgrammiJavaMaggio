5890aadcff8be4df96790097c67fedb67b362d8c
Carleoinserra <inserracarlo@gmail.com>
2026-05-08 14:31:33 +0200

    Add files via upload

---
+++ EsempioVariabili.java
@@ -0,0 +1,32 @@
+
+public class EsempioVariabili {
+	
+	public static void main(String[] args) {
+		
+		
+		// variabile di tipo intero uguale a 5
+		
+		int numInt = 5;
+		
+		int ris = 5 + 6;
+		int ris1 = 5 * 6;
+		int ris2 = 6 / 2;
+		int ris3 = 6 % 5;
+		String parola = "Il risultato della addizione è : " + ris;
+		String parola1 = "Il risultato della addizione è : " + (ris + ris1);
+		String parola2 = "i due risultati : " + ris  + " "  + ris1;
+		System.out.println(ris3);
+		System.out.println(parola2);
+		System.out.print(ris + " " + ris1 + " ");
+		System.out.print(ris2);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
