d8c7b44ef86ecfd6a3e9a538371dde6cc89dab36
Carleoinserra <inserracarlo@gmail.com>
2026-05-11 09:46:59 +0200

    Add files via upload

---
+++ CicliIterativa.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CicliIterativa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int scelta = 0;
+		
+		do {
+			
+			System.out.println("Premi qualsiasi tasto per continuare 0 per terminare");
+			scelta = input.nextInt();
+			
+			
+			
+		}
+		
+		while(scelta != 0);
+
+	}
+
+}
